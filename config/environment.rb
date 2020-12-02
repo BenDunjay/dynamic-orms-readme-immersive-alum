5174cbb482d555771f07554d9a08a720aa91f442
BenDunjay <bendunjay@outlook.com>
2020-12-02 21:52:12 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,7 @@ require "byebug"
 
 DB = { :conn => SQLite3::Database.new("db/songs.db") } # create database
 DB[:conn].execute("DROP TABLE IF EXISTS songs") # drop songs to avoid an error
-
+byebug
 sql = <<-SQL
   CREATE TABLE IF NOT EXISTS songs (
   id INTEGER PRIMARY KEY,
